5612c664ff129557fca7a0feafd9c96e4c501484
Jahidul Islam <jahidul.islam114593@gmail.com>
2024-12-01 09:20:47 +0600

    [added Testcpp]

--- test.cpp
+++ test.cpp
@@ -9,10 +9,31 @@ using namespace std;
 
 const int N = 1e5 + 10;
 
+class Student
+{
+public:
+    string name;
+    string program;
+    double studentId;
+    string session;
+
+    void changeProgram(string newProgram)
+    {
+        string program = newProgram;
+    }
+};
+
 int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
+
+    Student s1;
+    s1.name = "Jahidul Islam";
+    s1.program = "Bsc. in CSE";
+    s1.studentId = 2003810202074;
+    s1.session = "Fall 2020";
+    cout << s1.studentId << endl;
 
     return_loser;
 }
